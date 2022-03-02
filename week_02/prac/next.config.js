d60dc7fedaa2748bccd5727d84e8e48ec80193cb
Finley Sherwood <me@itsf.in>
2022-03-02 12:03:51 +1000

    Setup site for itsf.in domain

--- week_02/prac/next.config.js
+++ week_02/prac/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: 'https://itsf.in/CP3402/prac_01/',
+  trailingSlash: true
 }
 
 module.exports = nextConfig
